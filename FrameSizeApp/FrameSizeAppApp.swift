f25a2b9f648f14d822da408fda6eff054161270a
Bosco Domingo <bosco.domingo@tymit.io>
2021-04-19 15:50:55 +0200

    Initial Commit

---
+++ FrameSizeApp/FrameSizeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameSizeAppApp.swift
+//  FrameSizeApp
+//
+//  Created by Bosco Domingo on 19/04/21.
+//
+
+import SwiftUI
+
+@main
+struct FrameSizeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
